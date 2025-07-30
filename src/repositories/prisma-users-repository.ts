d80f9766442e660716a6a79800f59ac90b5be476
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2025-07-29 23:41:17 -0300

    refactor: implementar método de criação de usuário e aprimorar a estrutura do repositório

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -1,18 +1,24 @@
+import { Prisma, User } from "../generated/prisma";
 import { prisma } from "../lib/prima";
 import { RegisterInput } from "../services/register.service";
+import { UsersRepository } from "./users-repository";
 
-export class PrismaUsersRepository {
-    async createUser({
-        name, email, password
-    }: RegisterInput) {
-        const user = await prisma.user.create({
-            data: {
-                name,
-                email,
-                password,
-            },
-        });
-        
-        return user;
-    }
+export class PrismaUsersRepository implements UsersRepository {
+
+   async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = await prisma.user.create({
+        data
+    })
+    return user;
+   }
+
+   async findByEmail(email: string): Promise<User | null> {
+       const user = await prisma.user.findUnique({
+           where: {
+               email
+           }
+       });
+       return user;
+   }
+    
 }
