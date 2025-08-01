0854c2f660a0d6080cc060311e0405b6c82d0bac
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2025-08-01 18:36:45 -0300

    feat: adicionando novos testes

--- src/services/autenticate.spec.ts
+++ src/services/autenticate.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { InMemoryUsersRepository } from "../repositories/in-memory/in-memory-users-repository";
 import { AuthenticateService } from "./authenticate.service";
 import { hash } from "bcryptjs";
+import { InvalidCredentialsError } from "../errors/invalid-credentials.error";
 
 describe('Testes de Autenticação',  () => {
     it('Deve criar conseguir se autenticar com sucesso',  async () => {
@@ -23,4 +24,38 @@ describe('Testes de Autenticação',  () => {
         expect(user.id).toEqual(expect.any(String));
 
     })
+
+    it('Não deve fazer login com a senha errada', async () => {
+        const usersRepository = new InMemoryUsersRepository();
+        const authenticateUseCase = new AuthenticateService(usersRepository);
+
+        await usersRepository.create({
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password: await hash('password123', 6)
+        })
+
+        await expect(authenticateUseCase.execute({
+            email: 'john.doe@example.com',
+            password: 'wrong-password'
+        })).rejects.toBeInstanceOf(InvalidCredentialsError);
+    })
+
+    it('Não deve fazer login com email incorreto', async () => {
+
+        const usersRepository = new InMemoryUsersRepository();
+        const authenticateUseCase = new AuthenticateService(usersRepository);
+
+        await usersRepository.create({
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password: await hash('password123', 6)
+        })
+
+        await expect(authenticateUseCase.execute({
+            email: 'wrong.email@example.com',
+            password: 'password123'
+        })).rejects.toBeInstanceOf(InvalidCredentialsError);
+    })
+
 })
